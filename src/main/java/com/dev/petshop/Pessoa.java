cb3738a11ec790f5eed80beb4f1f03f15121feb3
Ines <ines.alessandra03@gmail.com>
2023-03-21 02:56:23 -0300

    Project created

---
+++ src/main/java/com/dev/petshop/Pessoa.java
@@ -0,0 +1,110 @@
+package com.dev.petshop;
+
+import java.time.Instant;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+public abstract class Pessoa {
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	private int enderecoId;
+	private String tipo; 
+	private String nome;
+	private Instant dataNascimento;
+	private String email;
+	private String senha;
+	private String telefone;
+	
+	public Pessoa(String nome, Instant dataNascimento, String email, String senha) {
+		this.nome = nome;
+		this.dataNascimento = dataNascimento;
+		this.email = email;
+		this.senha = senha;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getEnderecoId() {
+		return enderecoId;
+	}
+
+	public void setEnderecoId(int enderecoId) {
+		this.enderecoId = enderecoId;
+	}
+
+	public String getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Instant getDataNascimento() {
+		return dataNascimento;
+	}
+
+	public void setDataNascimento(Instant dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+
+	@Override
+	public int hashCode() {
+		// TODO Auto-generated method stub
+		return super.hashCode();
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		// TODO Auto-generated method stub
+		return super.equals(obj);
+	}
+
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return super.toString();
+	}
+
+}
---
+++ src/main/java/com/dev/petshop/controle/PessoaController.java
@@ -0,0 +1,11 @@
+package com.dev.petshop.controle;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("pessoa")
+
+public class PessoaController {
+
+}
